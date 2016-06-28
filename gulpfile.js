4bd37fb3e60b0de3117e1a84931812a9b93dcff6
mad-hu <450287549@qq.com>
2016-06-28 16:04:55 +0800

    0.1
    123123

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+//"gulp-concat": "^2.6.0",   js合并
+//    "gulp-minify-css": "^1.2.4",   css压缩
+//    "gulp-uglify": "^1.5.3" js压缩
+
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    rev = require('gulp-rev'),
+    compass = require('gulp-compass');
+
+///compass 任务。完成对sass文件的编译 对css文件的压缩 合并
+gulp.task('compass', function() {
+    gulp.src('./src/sass/*.scss')
+        .pipe(compass({
+            css: './src/css',
+            sass: './src/sass',
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./src/css/minincss'))
+        .pipe(concat('style.min.css'))                            //- 合并后的文件名
+        .pipe(gulp.dest('./dest/css/'));                               //- 输出文件本地       .pipe(rev()) //- 文件名加MD5后缀
+});
+
+
+//语法检查 任务1先检查语法
+gulp.task('jshint', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+
+//压缩 js  这里有问题 任务1 2 完成开始压缩js压缩后修改文件名
+gulp.task('minifyjs',['miniconcat'], function() {
+    return gulp.src('dest/js/main.js')      //需要操作的文件
+        .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
+        .pipe(uglify())    //压缩
+        .pipe(gulp.dest('dest/js'))       //输出到文件夹
+});
+
+
+//合并 js 这里有问题 任务完成1 2 完成后合并所有js
+gulp.task('miniconcat', function() {
+    return gulp.src('src/js/*.js')      //需要操作的文件
+        .pipe(concat('main.js'))    //合并所有js到main.js
+        .pipe(gulp.dest('dest/js'));  //输出
+});
+
+// 保证js语法检测完成，sass编译完成
+gulp.task('default',['jshint','compass'],function() {
+    gulp.start('miniconcat','minifyjs');
+    // 监听sass文件变化
+    gulp.watch('./src/sass/**/*.scss', function(){
+        gulp.start('compass');
+    });
+});
