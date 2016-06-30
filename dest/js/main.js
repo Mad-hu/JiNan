7448ae60a5c3323791f5ea79405af1ee3627fd19
qqlucky1 <806151843@qq.com>
2016-06-30 18:19:40 +0800

    ad2f454

--- dest/js/main.js
+++ dest/js/main.js
@@ -302,3 +302,95 @@ $(function(){
 });
 
  /*********************** new module Name End **************************************/
+
+/**
+ * Created by Caorh on 2016/6/30.
+ */
+$(function(){
+    linum = $('.mainlist li').length;//图片数量
+
+    w = linum/2 * 407;//ul宽度
+
+    $('.piclist').css('width', w + 'px');//ul宽度
+    $('.swaplist').html($('.mainlist').html());//复制内容
+
+    $('.og_next').click(function(){
+
+        if($('.swaplist,.mainlist').is(':animated')){
+            $('.swaplist,.mainlist').stop(true,true);
+        }
+        if($('.mainlist li').length>3){//多于3张图片
+            ml = parseInt($('.mainlist').css('left'));//默认图片ul位置
+            sl = parseInt($('.swaplist').css('left'));//交换图片ul位置
+            if(ml<=0 && ml>w*-1){//默认图片显示时
+                $('.swaplist').css({left: '1191px'});//交换图片放在显示区域右侧
+                $('.mainlist').animate({left: ml - 1221+ 'px'},'slow');//默认图片滚动
+                if(ml==(w-1221)*-1){//默认图片最后一屏时
+                    $('.swaplist').animate({left: '0px'},'slow');//交换图片滚动
+
+                }
+            }else{//交换图片显示时
+                $('.mainlist').css({left: '1191px'})//默认图片放在显示区域右
+                $('.swaplist').animate({left: sl - 1221+ 'px'},'slow');//交换图片滚动
+                if(sl==(w-1221)*-1){//交换图片最后一屏时
+                    $('.mainlist').animate({left: '0px'},'slow');//默认图片滚动
+                }
+            }
+        }
+    })
+    $('.og_prev').click(function(){
+
+        if($('.swaplist,.mainlist').is(':animated')){
+            $('.swaplist,.mainlist').stop(true,true);
+        }
+
+        if($('.mainlist li').length>3){
+            ml = parseInt($('.mainlist').css('left'));
+            sl = parseInt($('.swaplist').css('left'));
+            if(ml<=0 && ml>w*-1){
+                $('.swaplist').css({left: w * -1 + 'px'});
+                $('.mainlist').animate({left: ml +1221+ 'px'},'slow');
+                if(ml==0){
+                    $('.swaplist').animate({left: (w - 1221) * -1 + 'px'},'slow');
+                }
+            }else{
+                $('.mainlist').css({left: (w - 1221) * -1 + 'px'});
+                $('.swaplist').animate({left: sl + 1221+ 'px'},'slow');
+                if(sl==0){
+                    $('.mainlist').animate({left: '0px'},'slow');
+                }
+            }
+        }
+    });
+
+
+
+});
+
+
+
+/**
+ * Created by Caorh on 2016/6/30.
+ */
+
+$(document).ready(function () {
+
+    $('.zhsh_box_con li').hover(function () {
+        //  tol = $(".zhsh_box_con li").length;
+        //  alert(tol)
+        $(".text_block", this).stop().animate({top: '200px', opacity: '0.9'}, 500);
+    },function () {
+        $(".summary", this).stop().animate({top: '200px', opacity: '0.9'}, 500);
+
+    });
+});
+
+
+/*
+$(document).ready(function () {
+    $('.listimg').hover(function () {
+        $(".summary", this).stop().animate({top: '110px'}, {queue: false, duration: 180});
+    }, function () {
+        $(".summary", this).stop().animate({top: '165px'}, {queue: false, duration: 180});
+    });
+});*/
---
+++ src/js/yqgk.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Caorh on 2016/6/30.
+ */
+
+
+
+
+/*
+$(document).ready(function () {
+    $('.listimg').hover(function () {
+        $(".summary", this).stop().animate({top: '110px'}, {queue: false, duration: 180});
+    }, function () {
+        $(".summary", this).stop().animate({top: '165px'}, {queue: false, duration: 180});
+    });
+});*/
