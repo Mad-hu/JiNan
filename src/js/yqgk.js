c5e68f7de79bfc7121ab91fe760e61c3dc1f9d61
qqlucky1 <806151843@qq.com>
2016-07-01 10:05:15 +0800

    7.1

--- src/js/yqgk.js
+++ src/js/yqgk.js
@@ -5,11 +5,17 @@
 
 
 
-/*
-$(document).ready(function () {
-    $('.listimg').hover(function () {
-        $(".summary", this).stop().animate({top: '110px'}, {queue: false, duration: 180});
-    }, function () {
-        $(".summary", this).stop().animate({top: '165px'}, {queue: false, duration: 180});
+$(function(){
+    $(".zhsh_box_con li  .text_block").hide();
+    $(".zhsh_box_con li").hover(function(){
+        $(this).find(".text_block").stop().fadeTo(500,0.9)
+       $(this).find(".text").stop().animate({left:'0'}, {duration: 500})
+    },function(){
+        $(this).find(".text_block").stop().fadeTo(500,0)
+       $(this).find(".text").stop().animate({left:'318'}, {duration: "fast"})
+       $(this).find(".text").animate({left:'-318'}, {duration: 0})
     });
-});*/
+});
+
+
+
